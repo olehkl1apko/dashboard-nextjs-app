563b15a764382804bdf16f1f9af0b8ae4306831b
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-20 12:58:30 +0200

    ADD: mui & fix login cmp

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@mui/material";
 
 const Login = () => {
   const { data: session } = useSession();
@@ -6,44 +7,20 @@ const Login = () => {
   if (session) {
     return (
       <>
-        <p>Sign in as {session?.user?.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        <Button variant={"contained"} color={"error"} onClick={() => signOut()}>
+          Sign out
+        </Button>
       </>
     );
   }
   return (
     <>
       <h2>Please log in</h2> <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
+        Sign in
+      </Button>
     </>
   );
 };
 
 export default Login;
-
-// import { useSession, signIn, signOut } from "next-auth/react";
-// import Button from "@mui/material/Button";
-
-// const Login = () => {
-//   const { data: session } = useSession();
-
-//   if (session) {
-//     return (
-//       <>
-//         <Button variant={"contained"} color={"error"} onClick={() => signOut()}>
-//           Sign out
-//         </Button>
-//       </>
-//     );
-//   }
-//   return (
-//     <>
-//       <h2>Please log in</h2> <br />
-//       <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
-//         Sign in
-//       </Button>
-//     </>
-//   );
-// };
-
-// export default Login;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,6 @@ export default function Home() {
         <Header />
         <SideMenu />
         <Dashboard />
-        <Login />
       </main>
     </>
   );
