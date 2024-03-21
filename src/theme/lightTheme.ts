cda9f4b92615a06836f5c9e58be6bc6ddf3c4d52
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-21 16:15:41 +0200

    FIX: light theme

--- src/theme/lightTheme.ts
+++ src/theme/lightTheme.ts
@@ -4,13 +4,13 @@ const lightTheme: ThemeOptions = {
   palette: {
     mode: "light",
     primary: {
-      main: "#ffff00",
+      main: "#ffbb00",
     },
     secondary: {
       main: "#ff8400",
     },
     background: {
-      default: "#9e9e9e",
+      default: "#dddddd",
     },
   },
 };
